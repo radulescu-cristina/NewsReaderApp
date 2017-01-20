44e8376adecc2c43c3c0d525a3e726209b980f90
Cristina Radulescu <cristinaradulescu@192-168-0-101.rdsnet.ro>
2017-01-20 12:50:44 +0200

    Initial Commit

---
+++ NewsReaderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsReaderApp
+//
+//  Created by Cristina Radulescu on 1/20/17.
+//  Copyright © 2017 Cristina Radulescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
